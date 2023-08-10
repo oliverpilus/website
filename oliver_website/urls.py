26de6f5a15da14112c68efc939c0a1a7e2793453
Slav Pilus <slav.pilus@envoydigital.com>
2023-08-10 17:02:43 +0100

    added content to conversational_ai_chat

--- oliver_website/urls.py
+++ oliver_website/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home_page, name='home_page'),
     path('ai_chat', views.ai_chat, name='ai_chat'),
     path('ai_handle_input',views.ai_handle_input, name="ai_handle_input"),
+    path('conversational_ai_handle_input',views.conversational_ai_handle_input, name="ai_handle_input"),
     path('conversational_ai_chat',views.conversational_ai_chat, name="conversational_ai_chat")
 ]
--- oliver_website/views.py
+++ oliver_website/views.py
@@ -1,14 +1,47 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from dotenv import load_dotenv
+from langchain.memory import ConversationBufferMemory
 from langchain.schema import (
     AIMessage,
     HumanMessage,
     SystemMessage
 )
 from langchain.chat_models import ChatOpenAI
+from langchain.prompts import (
+    ChatPromptTemplate,
+    MessagesPlaceholder,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain.chains import LLMChain
 
 load_dotenv()
+
+
+llm = ChatOpenAI()
+
+# Prompt
+prompt = ChatPromptTemplate(
+    messages=[
+        SystemMessagePromptTemplate.from_template(
+            "You are a very rude chatbot having a conversation with a human."
+        ),
+        # The `variable_name` here is what must align with memory
+        MessagesPlaceholder(variable_name="chat_history"),
+        HumanMessagePromptTemplate.from_template("{question}")
+    ]
+)
+
+# Notice that we `return_messages=True` to fit into the MessagesPlaceholder
+# Notice that `"chat_history"` aligns with the MessagesPlaceholder name
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+conversation = LLMChain(
+    llm=llm,
+    prompt=prompt,
+    verbose=True,
+    memory=memory
+)
 
 
 def ai_chat(request):
@@ -27,5 +60,15 @@ def ai_handle_input(request):
     print(ai_output)
 
     return HttpResponse(ai_output.content, status=201)
+
+
+def conversational_ai_handle_input(request):
+    print(request.body)
+
+    ai_output = conversation({"question": request.body})
+    print(ai_output)
+    return HttpResponse(ai_output.get('text'), status=201)
+
+
 def conversational_ai_chat(request):
     return render(request, 'app/conversational_ai_chat.html')
